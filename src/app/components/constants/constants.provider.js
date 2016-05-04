295dea527bed988800ea71c64bda96d116da188c
Nelson Fernandez <nelson@netflux.com.ar>
2016-05-04 18:43:29 +0000

    move ooyala player assets locally

--- src/app/components/constants/constants.provider.js
+++ src/app/components/constants/constants.provider.js
@@ -10,7 +10,8 @@ class ConstantsProvider {
       ooplayer: {
         pcode: 'RyZ2cyOqrM7SQLfKy4Hys7rcjg9c',
         playerBrandingId: '24eb009549154660af0de0d4cd338508',
-        skin: {'config': '//player.ooyala.com/static/v4/stable/4.3.3/skin-plugin/skin.json'}
+        skin2: {'config': '//player.ooyala.com/static/v4/stable/4.3.3/skin-plugin/skin.json'},
+        skin: {'config': '/assets/skin.json'}
       }
     };
   }
