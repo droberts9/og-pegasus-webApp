23bc4fe70babcbcd7d8f2b4698a4f24acffd6b77
Nelson Fernandez <nelson@netflux.com.ar>
2016-05-09 21:51:33 +0000

    update footer and featured shows

--- src/app/categories/categories.controller.js
+++ src/app/categories/categories.controller.js
@@ -2,10 +2,11 @@ import _ from 'lodash';
 
 class CategoriesController {
 
-  constructor(apiService, $scope, $log, utils, series, featured) {
+  constructor(apiService, $scope, $log, $state, utils, series, featured) {
     'ngInject';
     this.api = apiService;
     this.$log = $log;
+    this.$state = $state;
     this.assets = {};
     this.utils = utils;
     this.series = [];
@@ -31,17 +32,23 @@ class CategoriesController {
     this.assets[categoryName] = {playList: []}
     // Retrieve it
     this.api.getCategory(categoryName).then( (category) => {
-
       if (category && category.assets.length > 0) {
         // If is a big carousel, slice the first on the slide
-        var slides = _.clone(category.assets, true);
         if (kind == 'big') {
-          this.assets[categoryName]['main'] = slides.shift();
+          this.assets[categoryName]['main'] = category.assets.shift();
         }
-        this.assets[categoryName]['playList'] = this.utils.groupOf(slides, 2);
+        this.assets[categoryName]['playList'] = this.utils.groupOf(category.assets, 2);
         this.assets[categoryName]['category'] = category;
       }
     });
+  }
+  
+  play(options) {
+    this.$log.warn('play', options);
+    this.$state.go(
+      'home.serie_show',
+      {serie: options.episode.serie_slug, season: options.episode.season_slug, show: options.episode.slug}
+    )
   }
 
 }
