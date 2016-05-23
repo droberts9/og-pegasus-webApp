97a532a44eb09105abb64a44c662d45771a8bbc9
Nelson Fernandez <nelson@netflux.com.ar>
2016-05-23 23:52:05 +0000

    update ad tag

--- src/app/index.run.js
+++ src/app/index.run.js
@@ -16,7 +16,7 @@ export function runBlock ($log, $anchorScroll, $window, DoubleClick) {
 
   DoubleClick.getSlot('div-gpt-ad-970x250-0').then(() => {
     $window.googletag.cmd.push(function() {
-      $window.googletag.defineOutOfPageSlot('/6747/saltwatertv', 'div-gpt-ad-intert-0').addService(googletag.pubads());
+      $window.googletag.defineOutOfPageSlot('/6747/saltwatertv', 'div-gpt-ad-intert-0').addService($window.googletag.pubads());
     });
   });
 
