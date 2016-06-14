927bb76bc8ba8f6d7ebe3a5e16c22f469633c28f
Nelson Fernandez <nelson@netflux.com.ar>
2016-06-14 22:14:46 +0000

    updat analytics code

--- src/app/components/constants/configfile.js
+++ src/app/components/constants/configfile.js
@@ -1,2 +1,2 @@
 export default angular.module("pegasusCms.config", [])
-.constant("EnvironmentConfig", {"appConfig":{"apiUrl":"http://stage.fish.sporkers.com/api/v1"},"ooplayer":{"pcode":"RyZ2cyOqrM7SQLfKy4Hys7rcjg9c","playerBrandingId":"24eb009549154660af0de0d4cd338508","skin2":{"config":"//player.ooyala.com/static/v4/stable/4.3.3/skin-plugin/skin.json"},"skin":{"config":"/assets/skin.json"},"debug":false}});
+.constant("EnvironmentConfig", {"appConfig":{"apiUrl":"http://prod.fish.sporkers.com/api/v1"},"ooplayer":{"pcode":"RyZ2cyOqrM7SQLfKy4Hys7rcjg9c","playerBrandingId":"24eb009549154660af0de0d4cd338508","skin2":{"config":"//player.ooyala.com/static/v4/stable/4.3.3/skin-plugin/skin.json"},"skin":{"config":"/assets/skin.json"},"debug":false}});
